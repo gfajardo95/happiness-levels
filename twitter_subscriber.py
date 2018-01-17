b1f899e472417a8636b28dac6df7a5c354982aff
Gabriel Fajardo <gfajardo95@gmail.com>
2018-01-17 12:42:22 -0500

    modify code execution so that it blocks for error handling

--- twitter_subscriber.py
+++ twitter_subscriber.py
@@ -29,18 +29,14 @@ def write_tweets_to_bq(dataset_id, table_id, tweets):
 # each list contains a dict with key data
 # list at each position gives us a dict with all the data points. That's the dict we want to save
 def write_tweets_to_bigquery(data):
-    if data:
-        tweets = []
-        stream = base64.urlsafe_b64decode(data)
-        twraw = json.loads(stream)
-        twmessages = twraw.get('messages')
-        for message in twmessages:
-            tweets.append(message['data'])
+    tweets = []
+    stream = base64.urlsafe_b64decode(data)
+    twraw = json.loads(stream)
+    twmessages = twraw.get('messages')
+    for message in twmessages:
+        tweets.append(message['data'])
 
-        write_tweets_to_bq(settings.DATASET_ID, settings.TABLE_ID, tweets)
-        return True
-    else:
-        return False
+    write_tweets_to_bq(settings.DATASET_ID, settings.TABLE_ID, tweets)
 
 
 def receive_tweets(project, subscription_name):
@@ -48,18 +44,23 @@ def receive_tweets(project, subscription_name):
     subscription_path = subscriber.subscription_path(
         project, subscription_name)
 
-    # ack should be the last thing you do
     def callback(message):
         print('Received message: {}'.format(message))
-        status = write_tweets_to_bigquery(message.data)
-        if status is True:
-            message.ack()
-        else:
-            print("received bad data")
+        write_tweets_to_bigquery(message.data)
+        message.ack()
 
-    subscriber.subscribe(subscription_path, callback=callback)
-
+    subscription = subscriber.subscribe(subscription_path, callback=callback)
     print('Listening for messages on {}'.format(subscription_path))
+
+    future = subscription.open(callback)
+    try:
+        future.result()
+    except Exception as e:
+        print(
+            'Listening for messages on {} threw an Exception: {}'.format(
+                subscription_name, e))
+        raise
+
     while True:
         time.sleep(60)
 
