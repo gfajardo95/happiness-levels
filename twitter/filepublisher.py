9c174f2f159e3b28af173fbbb1db02eab9585f8b
Gabriel Fajardo <gfajardo95@gmail.com>
2018-09-29 12:42:08 -0400

    publish bytes from a file filled with tweet data

---
+++ twitter/filepublisher.py
@@ -0,0 +1,16 @@
+import base64
+
+from google.cloud import pubsub
+
+import settings
+
+
+def read_and_publish_file():
+    client = pubsub.PublisherClient()
+    topic_path = client.topic_path(settings.PROJECT_NAME, settings.PUBSUB_TOPIC_NAME)
+    f = open('twitter/tmp/tweetbytes100.txt', 'rb')
+    client.publish(topic_path, data=f.read())
+
+
+if __name__ == '__main__':
+    read_and_publish_file()
