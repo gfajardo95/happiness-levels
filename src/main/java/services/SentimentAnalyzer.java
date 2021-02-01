151c4aed40ba19d8c7c7ccb22179196b4ce61aa1
Gabriel Fajardo <gfajardo95@gmail.com>
2021-02-01 12:15:26 -0500

    disable logging from Stanford CoreNLP

--- src/main/java/services/SentimentAnalyzer.java
+++ src/main/java/services/SentimentAnalyzer.java
@@ -7,6 +7,7 @@ import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.util.CoreMap;
+import edu.stanford.nlp.util.logging.RedwoodConfiguration;
 import org.apache.commons.math3.util.Precision;
 
 import java.util.List;
@@ -15,17 +16,21 @@ import java.util.Properties;
 public class SentimentAnalyzer {
 
     public double getSentimentFromText(String text) {
+        RedwoodConfiguration.current().clear().apply();  // disable logging from Stanford CoreNLP
+
         double weightedSentiment = 0;
 
         if (text != null && text.length() > 0) {
             Properties props = new Properties();
             props.setProperty("annotators", "tokenize, ssplit, parse, sentiment");
             StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+            
             Annotation doc = new Annotation(text);
             pipeline.annotate(doc);
 
             int sentiment = 0, sentimentWeights = 0, fragLength = 0, totalLength = 0;
             List<CoreMap> sentences = doc.get(CoreAnnotations.SentencesAnnotation.class);
+
             /*a weighted sentiment for the text is calculated based on each sentence's sentiment and word count*/
             for (CoreMap sentence : sentences) {
                 Tree tree = sentence.get(SentimentCoreAnnotations.SentimentAnnotatedTree.class);
@@ -36,9 +41,10 @@ public class SentimentAnalyzer {
                 totalLength += fragLength;
                 sentimentWeights += fragLength * sentiment;
             }
+
             weightedSentiment = (double)sentimentWeights / (double)totalLength;
         }
+
         return Precision.round(weightedSentiment, 4);
     }
-
 }
