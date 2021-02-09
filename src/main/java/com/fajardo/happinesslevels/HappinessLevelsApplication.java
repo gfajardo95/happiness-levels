622b479c3dcbbbc84c097e45d25e3196b2a7adfa
Gabriel Fajardo <gfajardo95@gmail.com>
2021-02-09 13:17:19 -0500

    create pipeline properties configuration

--- src/main/java/com/fajardo/happinesslevels/HappinessLevelsApplication.java
+++ src/main/java/com/fajardo/happinesslevels/HappinessLevelsApplication.java
@@ -2,8 +2,10 @@ package com.fajardo.happinesslevels;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(PipelineProperties.class)
 public class HappinessLevelsApplication {
     
     public static void main(String args[]) {
