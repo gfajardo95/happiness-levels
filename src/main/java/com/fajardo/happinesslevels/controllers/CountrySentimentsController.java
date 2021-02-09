b72f065e40e1486dabe68be10095304525a05ca7
Gabriel Fajardo <gfajardo95@gmail.com>
2021-02-09 13:18:08 -0500

    conditionally run pipeline based on configuration

--- src/main/java/com/fajardo/happinesslevels/controllers/CountrySentimentsController.java
+++ src/main/java/com/fajardo/happinesslevels/controllers/CountrySentimentsController.java
@@ -3,24 +3,27 @@ package com.fajardo.happinesslevels.controllers;
 import java.io.File;
 import java.io.IOException;
 
+import com.fajardo.happinesslevels.PipelineProperties;
 import com.fajardo.happinesslevels.models.CountrySentiment;
 import com.fajardo.happinesslevels.models.CountrySentimentRequest;
 import com.google.gson.Gson;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gcp.pubsub.reactive.PubSubReactiveFactory;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
 @Slf4j
+@AllArgsConstructor
 @Controller
 public class CountrySentimentsController implements CountrySentiments {
 
-    @Autowired
-    PubSubReactiveFactory reactiveFactory;
+    private final PubSubReactiveFactory reactiveFactory;
+
+    private final PipelineProperties pipelineProps;
 
     @MessageMapping("country-sentiments")
     @Override
@@ -46,14 +49,17 @@ public class CountrySentimentsController implements CountrySentiments {
     }
 
     private void runPipeline() {
-        try {
-            ProcessBuilder processBuilder = new ProcessBuilder()
-                    .command("zsh", "-c", ". venv/bin/activate && python3 run_pipeline.py")
-                    .directory(new File(System.getProperty("user.dir"))).redirectErrorStream(true);
-
-            processBuilder.inheritIO().start();
-        } catch (IOException e) {
-            log.error("Error running the pipeline", e);
+        if (!pipelineProps.isTesting()) {
+            try {
+                new ProcessBuilder()
+                        .command("zsh", "-c", ". venv/bin/activate && python3 run_pipeline.py")
+                        .directory(new File(System.getProperty("user.dir")))
+                        .redirectErrorStream(true)
+                        .inheritIO()
+                        .start();
+            } catch (IOException e) {
+                log.error("Error running the pipeline", e);
+            }
         }
     }
 }
